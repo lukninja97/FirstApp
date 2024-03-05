aee73c95f63e3f08ed87dac1ee5c4d1d8484e453
lukninja97 <lucassilvasouto@gmail.com>
2024-03-05 11:29:34 -0300

    criar um layout para calcular a idade.
    criar um layout para validar um email e telefone.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -38,6 +41,16 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
+
+    //Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("androidx.annotation:annotation:1.6.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -2,10 +2,74 @@ package com.example.firstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.firstapp.databinding.ActivityMainBinding
+import com.example.firstapp.databinding.TelaLinearBinding
+import java.time.Clock
+import java.time.LocalDate
+import java.time.LocalDateTime
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        binding.btnEnviar.setOnClickListener {
+//            var nome = binding.edtNome.editableText.toString()
+//
+//            binding.tvNome.text = "Nome: " + nome
+//
+//            var anoNascimento = binding.edtAnoNascimento.editableText.toString()
+//            val anoAtual = LocalDateTime.now().year
+//            var idade = anoAtual - anoNascimento.toInt()
+//
+//            binding.tvIdade.text = "Idade: ${idade}"
+//        }
+
+        binding.btnEnviar.setOnClickListener {
+            // Coleta as informações
+            val telefone = binding.edtTelefone.editableText.toString()
+            val email = binding.edtEmail.editableText.toString()
+
+            //Verificar telefone
+            if (telefone.length == 11){
+                binding.tvTelefone.text = "Telefone válido"
+            } else {
+                binding.tvTelefone.text = "Telefone inválido"
+            }
+
+            //Verificar se o email tem @ e .com
+            if (email.contains("@") && email.contains(".com")){
+                val dominio = email.substringAfter("@")
+                binding.tvEmail.text = "Email valido, dominio " + dominio
+            } else {
+                binding.tvEmail.text = "Email inválido"
+            }
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+    private fun emailEstaValido(email: String): Boolean {
+        val arroba = email.find { it == '@' }
+
+        return arroba == '@'
+    }
+
+    fun getAnoAtual(): Int {
+        return LocalDate.now().year
     }
 }
